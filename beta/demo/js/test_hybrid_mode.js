a566c295345344d7f720b3a89b8d15e464e5df39
MoonStorm <MoonStorm@gmail.com>
2014-07-10 16:04:01 +0100

    hybrid mode test added

---
+++ beta/demo/js/test_hybrid_mode.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="../../src/external/typings/angularjs/angular.d.ts" />
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var TrNgGridDemo;
+(function (TrNgGridDemo) {
+    var HybridModeTestController = (function (_super) {
+        __extends(HybridModeTestController, _super);
+        function HybridModeTestController($scope, $window, $timeout) {
+            _super.call(this, $scope, $window, $timeout);
+            $scope.myPageItemsCount = 5;
+            $scope.myCurrentPage = 2;
+
+            this.$scope.onServerSideItemsRequested = function (currentPage, filterBy, filterByFields, orderBy, orderByReverse) {
+                $scope.requestedPageNumber = currentPage;
+                $scope.generateItems($scope.myPageItemsCount, $scope.myItemsTotalCount);
+                $scope.myItemsTotalCount = 200;
+            };
+        }
+        return HybridModeTestController;
+    })(TrNgGridDemo.TestController);
+    TrNgGridDemo.HybridModeTestController = HybridModeTestController;
+})(TrNgGridDemo || (TrNgGridDemo = {}));
+//# sourceMappingURL=test_hybrid_mode.js.map
