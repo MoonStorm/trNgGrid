65cfc99d256e50d1f721aa51e1612aa8f10c1fa9
MoonStorm <MoonStorm@gmail.com>
2014-07-25 01:53:33 +0100

    bootswatch theme urls fixed

--- release/demo/js/index.ts
+++ release/demo/js/index.ts
@@ -289,10 +289,11 @@ module TrNgGridDemo{
         setupThemeUrl() {
             var themeUrl;
             if (this.$scope.ui.themeVersion == "latest") {
+                // CROSS ORIGIN problem in FF, don't use
                 themeUrl = "//thomaspark.github.io/bootswatch/" + this.$scope.ui.theme + "/bootstrap.css";
             }
             else {
-                themeUrl = "//netdna.bootstrapcdn.com/bootswatch/" + this.$scope.ui.themeVersion + "/" + this.$scope.ui.theme + "/bootstrap.min.css";
+                themeUrl = "//maxcdn.bootstrapcdn.com/bootswatch/" + this.$scope.ui.themeVersion + "/" + this.$scope.ui.theme + "/bootstrap.min.css";
             }
             this.$scope.ui.themeUrl = this.$sce.trustAsResourceUrl(themeUrl);
         }
