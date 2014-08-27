288e6e8675421a962f3d19d85704bbd65f2298a7
Jhonny Everson <khronnuz@gmail.com>
2014-08-27 00:01:44 -0300

    Adds ability to disable column filtering without disabling global filtering or having to disable them individually.

--- release/src/js/trNgGrid.ts
+++ release/src/js/trNgGrid.ts
@@ -151,6 +151,7 @@ module TrNgGrid{
         currentPage: number;
         totalItems: number;
         enableFiltering: boolean;
+        enableColumnFiltering: boolean;
         enableSorting: boolean;
         selectionMode: string;
         onDataRequired: (gridOptions: IGridOptions) => void;
@@ -467,6 +468,7 @@ module TrNgGrid{
                 currentPage:0,
                 totalItems:null,
                 enableFiltering:true,
+                enableColumnFiltering:true,
                 enableSorting:true,
                 selectionMode:SelectionMode[SelectionMode.MultiRow],
                 onDataRequiredDelay:1000
@@ -1019,6 +1021,7 @@ module TrNgGrid{
                         currentPage: '=?',
                         totalItems: '=?',
                         enableFiltering: '=?',
+                        enableColumnFiltering: '=?',
                         enableSorting: '=?',
                         enableSelections: '=?', // deprecated
                         enableMultiRowSelections: '=?', // deprecated
@@ -1477,7 +1480,7 @@ module TrNgGrid{
         }
         if (!$templateCache.get(TrNgGrid.columnFilterTemplateId)) {
             $templateCache.put(TrNgGrid.columnFilterTemplateId,
-                '<div ng-show="(gridOptions.enableFiltering&&columnOptions.enableFiltering!==false)||columnOptions.enableFiltering" class="' + TrNgGrid.columnFilterCssClass + '">'
+                '<div ng-show="(gridOptions.enableFiltering&&gridOptions.enableColumnFiltering!==false&&columnOptions.enableFiltering!==false)||columnOptions.enableFiltering" class="' + TrNgGrid.columnFilterCssClass + '">'
                 + ' <div class="' + TrNgGrid.columnFilterInputWrapperCssClass + '">'
                 + '   <input class="form-control input-sm" type="text" ng-model="columnOptions.filter" ng-keypress="speedUpAsyncDataRetrieval($event)"></input>'
                 + ' </div>'
