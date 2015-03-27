f95ce1279e5a726ff33be79fe92d6e110811a9b1
MoonStorm <MoonStorm@gmail.com>
2015-03-27 00:16:21 -0600

    more work on alpha, detailed explanation for angular version requirements on demo

--- Scripts/typings/angularjs/angular.d.ts
+++ Scripts/typings/angularjs/angular.d.ts
@@ -1081,7 +1081,7 @@ declare module angular {
     // This corresponds to $transclude (and also the transclude function passed to link).
     interface ITranscludeFunction {
         // If the scope is provided, then the cloneAttachFn must be as well.
-        (scope: IScope, cloneAttachFn: ICloneAttachFunction, futureParent:IAugmentedJQuery): IAugmentedJQuery;
+        (scope: IScope, cloneAttachFn: ICloneAttachFunction, futureParent?:IAugmentedJQuery): IAugmentedJQuery;
         // If one argument is provided, then it's assumed to be the cloneAttachFn.
         (cloneAttachFn?: ICloneAttachFunction): IAugmentedJQuery;
     }
